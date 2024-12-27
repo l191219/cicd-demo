ee290516885c52c1c2c0c390b6b8b12ec57c6e57
Lihang <1912192469@qq.com>
2024-12-27 22:04:40 +0800

    你航哥太帅了

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
     <h1>厉不厉害你航哥！</h1>
+    <h1>你航哥太帅了</h1>
     <chartPie :series-data="dataList" :extra-option="extraOption" />
   </div>
   <!-- <HelloWorld msg="Vite + Vue" /> -->
