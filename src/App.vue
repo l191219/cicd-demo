9e02b293e6f39dd085f3376c5c02e2eea0481c10
Lihang <1912192469@qq.com>
2025-01-02 09:31:59 +0800

    a

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
     <h1>厉不厉害你航哥！</h1>
     <h1>你航哥太帅了</h1>
     <h1>123456</h1>
+    <h1>2025/1/2</h1>
     <chartPie :series-data="dataList" :extra-option="extraOption" />
   </div>
   <HelloWorld msg="Vite + Vue" />
