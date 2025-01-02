2144d55a2f1cfcf3ca8d45c9306e1d2665456aa8
Lihang <1912192469@qq.com>
2025-01-02 10:00:49 +0800

    提交1.2

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <h1>今天是一月二日</h1>
     <chartPie :series-data="dataList" :extra-option="extraOption" />
   </div>
   <HelloWorld msg="Vite + Vue" />
